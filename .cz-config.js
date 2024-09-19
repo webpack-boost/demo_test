70d0c689db042ab84c9e38ddd5e618ede450e926
maomiao <maomiao@mrray.cn>
2024-09-19 19:45:43 +0800

    feat: add new package

---
+++ .cz-config.js
@@ -0,0 +1,71 @@
+module.exports = {
+  types: [
+    {
+      value: "feat",
+      name: "feat: ✨新功能",
+    },
+    {
+      value: "fix",
+      name: "fix: 🐛修复bug",
+    },
+    {
+      value: "init",
+      name: "init: 🎉初始化",
+    },
+    {
+      value: "docs",
+      name: "docs: 📝文档变更",
+    },
+    {
+      value: "style",
+      name: "style: 💄代码的样式美化",
+    },
+    {
+      value: "refactor",
+      name: "refactor: ♻️重构",
+    },
+    {
+      value: "perf",
+      name: "perf: ⚡️性能优化",
+    },
+    {
+      value: "test",
+      name: "test: ✅测试",
+    },
+    {
+      value: "revert",
+      name: "revert: ⏪️回退",
+    },
+    {
+      value: "build",
+      name: "build: 📦️打包",
+    },
+    {
+      value: "chore",
+      name: "chore: 👷构建/工程依赖/工具",
+    },
+    {
+      value: "ci",
+      name: "ci: 💚CI环境相关代码变更",
+    },
+  ],
+  messages: {
+    type: "请选择提交类型(必填)",
+    scope: "请输入文件修改范围(可选)",
+    customScope: "请输入文件修改范围(可选)",
+    subject: "请简要描述提交(必填)",
+    body: "请输入详细描述(可选)",
+    breaking: "列出任何BREAKING CHANGES(破坏性修改)(可选)",
+    footer: "请输入要关闭的issue(可选)",
+    confirmCommit: "确定提交此说明吗？",
+  },
+  scopes: [
+    { name: "normal file" },
+    {
+      name: "config",
+    },
+  ],
+  allowCustomScopes: true,
+  allowBreakingChanges: ["feat", "fix", "perf"], // 当提交类型为feat、fix、pref时才有破坏性修改选项
+  subjectLimit: 72,
+};
---
+++ packages/commitlint-config/README.md
@@ -0,0 +1,11 @@
+# `@leisuremao/commitlint-config`
+
+> TODO: description
+
+## Usage
+
+```
+const commitlintConfig = require('@leisuremao/commitlint-config');
+
+// TODO: DEMONSTRATE API
+```
